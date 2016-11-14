379408cba7021ea53eec5583244e0156e235ff97
陈强 <qiang.chen@utsoft.cn>
2016-11-14 17:22:07 +0800

    Initial Commit

---
+++ JCAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JCAlertView
+//
+//  Created by jon on 16/11/14.
+//  Copyright © 2016年 jon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JCAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JCAlertView
+//
+//  Created by jon on 16/11/14.
+//  Copyright © 2016年 jon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
